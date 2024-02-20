be76154f2427f23468025341511c330e1b55825e
athphane <athphane@gmail.com>
2024-02-20 22:45:23 +0500

    Reload

--- order.sh
+++ order.sh
@@ -1,11 +1,12 @@
 a=1
 
-for file in `find . -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.jpeg \)`; do
+for file in $(find . -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.jpeg \)); do
   extension=${file##*.}
-  new=$(printf "%04d.$extension" "$a") #04 pad to length of 4
+  directory=$(dirname "$file")
+  new=$(printf "%s/%04d.$extension" "$directory" "$a") # Include directory in the output
   
-  echo $new
-  mv -i -- "$file" "$new"
+  echo "$new"
+  mv -n -- "$file" "$new"
   let a=a+1
 done
 
